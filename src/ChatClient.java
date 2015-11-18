c2898e1958f6a20100d43acb642772edd0306af6
sherlockhomeless <envindavsorg@gmail.com>
2015-11-18 23:48:41 +0100

    bisschen kommentiert, was wir noch checken müssen:
    -brauchen wir Semaphore für den Thread? Klingt ein bisschen so, aber
    glaube kann man weglassen "beliebig viele Clients"
    -ChatHistory anzeigen fehlt noch, hab ich schon nen Teil eingebaut, aber
    geht noch nicht

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,6 +1,3 @@
-import com.sun.deploy.util.SessionState;
-import com.sun.security.ntlm.Client;
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -9,160 +6,213 @@ import java.net.Socket;
 import java.util.Scanner;
 
 /**
+ * Client über den sich ein Nutzer bei Angabe von PortNr. & Host bei einem Chat-Client anmelden kann 
+ * 
  * Created by DucNguyenMinh on 17.11.15.
  */
 public class ChatClient
 {
     /* Portnummer */
-    private final int serverPort;
-
+    private final int        serverPort;
+                             
     /* Hostname */
-    private final String hostname;
-
-    private final String clientName;
-
-    private Socket clientSocket;
-    private boolean isLoggedOut;
-    private DataOutputStream outToServer; // Ausgabestream zum Server
-    private BufferedReader inFromServer; // Eingabestream vom Server
-
-    private boolean serviceRequested = true; // ChatClient beenden?
-
+    private final String     hostname;
+    /* Name des Clients für Serveridentifizierung */
+    private final String     clientName;
+    /* Socket für Kommunikation mit ChatServer */
+    private Socket           clientSocket;
+    /* Flag, falls Nutzer sind ausloggen möchste */
+    private boolean          isLoggedOut;
+    /* Ausgabestream zum Server */
+    private DataOutputStream outToServer;
+    /* Eingabestream vom Server */
+    private BufferedReader   inFromServer;
+    
+                                              
+    /**
+     * 
+     * @param hostname
+     *            Hostname des ChatServers
+     * @param serverPort
+     *            Port, auf dem Server lauscht
+     * @param userName
+     *            Angezeigte Name des Nutzers im Chat
+     */
     public ChatClient(String hostname, int serverPort, String userName)
     {
         this.serverPort = serverPort;
         this.hostname = hostname;
         this.clientName = userName;
     }
-
-
+    
+    /**
+     * Startet den Client inklusive Handshake
+     */
     public void startJob()
     {
-
+        
         Scanner inFromUser;
         String chatMsg = "";
-        try {
+        try
+        {
             clientSocket = new Socket(hostname, serverPort);
-            System.out.println("Connection established " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
-
-            this.inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            this.outToServer = new DataOutputStream(clientSocket.getOutputStream());
-
+            System.out.println(
+                    "Connection established " + clientSocket.getInetAddress()
+                            + ":" + clientSocket.getPort());
+                            
+            this.inFromServer = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            this.outToServer = new DataOutputStream(
+                    clientSocket.getOutputStream());
+                    
             new ListenFromServer().start();
-
-            sendToServer(this.clientName);
-
+            
+            sendToServer(clientName);
+            
             isLoggedOut = false;
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             System.out.println("Error connecting to server: " + e);
         }
-
+        
         inFromUser = new Scanner(System.in);
-
-        while (!isLoggedOut) {
+        
+        /* Schleife die Nutzereingaben in Stdin an den Server schickt */
+        while (!isLoggedOut)
+        {
             System.out.print("> ");
-
-           /* String vom Benutzer (Konsoleneingabe) holen */
+            
+            /* String vom Benutzer (Konsoleneingabe) holen */
             chatMsg = inFromUser.nextLine();
-
-            if (chatMsg.equalsIgnoreCase("LOGOUT.")) {
+            
+            if (chatMsg.equalsIgnoreCase("LOGOUT."))
+            {
                 isLoggedOut = true;
-//                logOut();
+                // logOut();
             }
-
+            
             sendToServer(chatMsg);
-
+            
         }
+        inFromUser.close();
         logOut();
     }
-
+    
+    /**
+     * Schiebt die übergebene Nachricht mit dem Socket zum Server
+     * 
+     * @param request
+     */
     private void sendToServer(String request)
     {
-        try {
+        try
+        {
             outToServer.writeBytes(request + '\r' + '\n');
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             System.out.println("Exception writing to server: " + e);
         }
     }
-
-
+    
+    /**
+     * Schließt die Input/Output-Streams sowie das Socket selbst
+     */
     private void logOut()
     {
-        try {
+        try
+        {
             if (inFromServer != null)
                 inFromServer.close();
-            if (outToServer != null) outToServer.close();
-            if (clientSocket != null) clientSocket.close();
-        } catch (IOException e) {
+            if (outToServer != null)
+                outToServer.close();
+            if (clientSocket != null)
+                clientSocket.close();
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
-
-
+    /**
+     * Main-Methode. Standardmäßig werden Port, Host & Username als args-Parameter übergeben
+     * @param args
+     */
     public static void main(String[] args)
     {
         Scanner inFromUser = new Scanner(System.in);
-        String inStr = "";
-
+        
         /* Default Werte */
         int portNummer = 56789;
         String hostname = "localhost";
         String username = "someone";
-
-        switch (args.length) {
-            // > javac Client username portNumber serverAddr
-            case 3:
-                hostname = args[2];
-                // > javac Client username portNumber
-            case 2:
-                try {
-                    portNummer = Integer.parseInt(args[1]);
-                } catch (Exception e) {
-                    System.out.println("Invalid port number.");
-                    System.out.println("Usage is: > java Client [username] [portNumber] [serverAddress]");
-                    return;
-                }
-                // > javac Client username
-            case 1:
-                username = args[0];
-                // > java Client
-                break;
-            // invalid number of arguments
-            case 0:
-
-                break;
-            default:
-                System.out.println("Usage is: > java Client [username] [portNumber] {serverAddress]");
+        
+        switch (args.length)
+        {
+        // > javac Client username portNumber serverAddr
+        case 3:
+            hostname = args[2];
+            // > javac Client username portNumber
+        case 2:
+            try
+            {
+                portNummer = Integer.parseInt(args[1]);
+            }
+            catch (Exception e)
+            {
+                System.out.println("Invalid port number.");
+                System.out.println(
+                        "Usage is: > java Client [username] [portNumber] [serverAddress]");
                 return;
+            }
+            // > javac Client username
+        case 1:
+            username = args[0];
+            // > java Client
+            break;
+        // invalid number of arguments
+        // case 0:
+        //
+        // break;
+        default:
+            System.out.println(
+                    "Usage is: > java Client [username] [portNumber] {serverAddress]");
+            return;
         }
-
-
+        
         ChatClient client = new ChatClient(hostname, portNummer, username);
-
+        
         client.startJob();
+        inFromUser.close();
+        
     }
-
-
+    
     /**
-     *
+     * Thread-Klasse, die das Lesen vom Chatserver seperat übernimmt
      */
     class ListenFromServer extends Thread
     {
         @Override
         public void run()
         {
-            while (!isLoggedOut) {
-                try {
+            while (!isLoggedOut)
+            {
+                try
+                {
                     String message = inFromServer.readLine();
-                    if (message != null) {
+                    if (message != null)
+                    {
                         System.out.println(message);
                         System.out.print(">");
                     }
-                } catch (IOException e) {
-                    System.out.print("Server has closed connection");
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
                 }
             }
         }
-
+        
     }
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,188 +1,263 @@
-import sun.misc.Cleaner;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.concurrent.Semaphore;
 
 /**
+ * Chat-Server der Chat-Raum mit mehreren Nutzern zur Verfügung stellt.
+ * 
  * Created by DucNguyenMinh on 18.11.15.
  */
 public class ChatServer
 {
     /* TCP-Server, der Verbindungsanfragen entgegennimmt */
-
-    /* Semaphore begrenzt die Anzahl parallel laufender Worker-Threads  */
-    public Semaphore workerThreadsSem;
-
+    
+    /* Semaphore begrenzt die Anzahl parallel laufender Worker-Threads */
+    public Semaphore                workerThreadsSem;
+                                    
     /* Portnummer */
-    public final int serverPort;
-
+    public final int                serverPort;
+                                    
     /* Anzeige, ob der Server-Dienst weiterhin benoetigt wird */
-    public boolean serviceRequested = true;
-
+    public boolean                  serviceRequested = true;
+                                                     
+    public LinkedList<String>       chatHistory;
     private ArrayList<ClientThread> clients;
-
-    private SimpleDateFormat dateFormat;
-
-    /* Konstruktor mit Parametern: Server-Port, Maximale Anzahl paralleler Worker-Threads*/
-    public ChatServer(int serverPort) {
+                                    
+    private SimpleDateFormat        dateFormat;
+                                    
+    /**
+     * 
+     * @param serverPort
+     */
+    public ChatServer(int serverPort)
+    {
         this.serverPort = serverPort;
-//        this.workerThreadsSem = new Semaphore(maxThreads);
+        this.workerThreadsSem = new Semaphore(2);
+        this.chatHistory = new LinkedList<String>();
         this.clients = new ArrayList<>();
         this.dateFormat = new SimpleDateFormat("HH:mm:ss");
     }
-
+    
+    public LinkedList<String> getChatHistory()
+    {
+        return chatHistory;
+    }
+    
     /**/
-    public synchronized void broadcast(String message){
+    public synchronized void broadcast(String message)
+    {
         String time = dateFormat.format(new Date());
         String msg = time + " " + message + "\n";
-
+        
+        chatHistory.add(message);
         System.out.print(msg);
-
-        for(ClientThread client : clients ){
+        
+        for (ClientThread client : clients)
+        {
             client.writeToClient(msg);
         }
     }
-    public synchronized void addClient(ClientThread clientThread){
+    
+    //TODO geht noch nicht
+    public synchronized void printChatHistory(ClientThread clientthread)
+    {
+        for (String s : chatHistory)
+            clientthread.writeToClient(s);
+    }
+    
+    public synchronized void addClient(ClientThread clientThread)
+    {
         clients.add(clientThread);
     }
-
-    public synchronized void removeClient(ClientThread clientThread) {
+    
+    public synchronized void removeClient(ClientThread clientThread)
+    {
+        workerThreadsSem.release();
         clients.remove(clientThread);
     }
-
-    public void startServer() {
+    
+    public void startServer()
+    {
         serviceRequested = true;
         ServerSocket welcomeSocket; // TCP-Server-Socketklasse
         Socket clientSocket; // TCP-Standard-Socketklasse
-
+        
         int nextThreadNumber = 0;
-
-        try {
-         /* Server-Socket erzeugen */
+        
+        try
+        {
+            /* Server-Socket erzeugen */
             welcomeSocket = new ServerSocket(serverPort);
-
-            while (serviceRequested) {
-//                workerThreadsSem.acquire();  // Blockieren, wenn max. Anzahl Worker-Threads erreicht
-
-                System.out.println("TCP Server is waiting for connection - listening TCP port " + serverPort);
-            /*
-             * Blockiert auf Verbindungsanfrage warten --> nach Verbindungsaufbau
-             * Standard-Socket erzeugen und an connectionSocket zuweisen
-             */
+            
+            while (serviceRequested)
+            {
+                // workerThreadsSem.acquire(); // Blockieren, wenn max. Anzahl
+                // Worker-Threads erreicht
+                
+                System.out.println(
+                        "TCP Server is waiting for connection - listening TCP port "
+                                + serverPort);
+                /*
+                 * Blockiert auf Verbindungsanfrage warten --> nach
+                 * Verbindungsaufbau Standard-Socket erzeugen und an
+                 * connectionSocket zuweisen
+                 */
+                workerThreadsSem.acquire();
                 clientSocket = welcomeSocket.accept();
-
-            /* Neuen Arbeits-Thread erzeugen und die Nummer, den Socket sowie das Serverobjekt uebergeben */
+                /*
+                 * Neuen Arbeits-Thread erzeugen und die Nummer, den Socket
+                 * sowie das Serverobjekt uebergeben
+                 */
                 ClientThread clientThread = new ClientThread(clientSocket);
                 addClient(clientThread);
                 clientThread.start();
             }
-
-            try {
+            
+            try
+            {
                 welcomeSocket.close();
-                for(ClientThread clientThread : clients){
+                for (ClientThread clientThread : clients)
+                {
                     clientThread.closeSocket();
                 }
-            }catch (Exception e){
-
             }
-        } catch (Exception e) {
+            catch (Exception e)
+            {
+            
+            }
+        }
+        catch (Exception e)
+        {
             System.err.println(e.toString());
         }
-
-
-
+        
     }
-
-
-    public static void main(String[] args) {
-      /* Erzeuge Server und starte ihn */
+    
+    public static void main(String[] args)
+    {
+        /* Erzeuge Server und starte ihn */
         ChatServer myServer = new ChatServer(56789);
         myServer.startServer();
     }
-
-
-    class ClientThread extends Thread {
-
-        private Socket clientSocket;
-
-        private BufferedReader inFromClient;
-
+    
+    class ClientThread extends Thread
+    {
+        
+        private Socket           clientSocket;
+                                 
+        private BufferedReader   inFromClient;
+                                 
         private DataOutputStream outToClient;
-
-        String username;
-
-        String date;
-
-
+                                 
+        String                   username;
+                                 
+        String                   date;
+                                 
         public ClientThread(Socket clientSk)
         {
             this.clientSocket = clientSk;
-
-            try {
-                this.inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                this.outToClient = new DataOutputStream(clientSk.getOutputStream());
-                this.username = inFromClient.readLine();
-                System.out.println(username + "has just joined chatroom.");
-
-
-            } catch (IOException e) {
-                System.out.println("Exception creating new Input/output Streams: " + e);
+            
+            try
+            {
+                inFromClient = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+                outToClient = new DataOutputStream(clientSk.getOutputStream());
+                username = inFromClient.readLine();
+                System.out.println(username + "has just joined the chatroom.");
+                
+            }
+            catch (IOException e)
+            {
+                System.out.println(
+                        "Exception creating new Input/output Streams: " + e);
                 return;
             }
-
+            
             date = new Date().toString() + "\n";
         }
-
+        
         @Override
-        public void run(){
+        public void run()
+        {
             broadcast(username + "has just joined chatroom.");
             boolean isLogoutRequested = false;
-            String chatMessage = "" ;
-
-            while(!isLogoutRequested){
-                try {
+            String chatMessage = "";
+            
+            while (!isLogoutRequested)
+            {
+                try
+                {
                     chatMessage = inFromClient.readLine();
                 }
-                catch (IOException e) {
-                    System.out.println(username + " Exception reading Streams: " + e);
+                catch (IOException e)
+                {
+                    System.out.println(
+                            username + " Exception reading Streams: " + e);
                     break;
                 }
-
-                if(chatMessage.equalsIgnoreCase("LOGOUT.")){
-                    String msg = username + " has left the room with LOGOUT. message.";
+                
+                if (chatMessage.equalsIgnoreCase("LOGOUT."))
+                {
+                    String msg = username + " has left the room.";
                     System.out.println(msg);
                     broadcast(msg);
                     isLogoutRequested = true;
-                }else{
+                }
+                else if (chatMessage.equals("HISTORY."))
+                {
+                    printChatHistory(this);
+                }
+                else
+                {
                     broadcast(username + ": " + chatMessage);
                 }
             }
-
+            
             removeClient(this);
             closeSocket();
         }
-
-        public void writeToClient(String message){
-            try{
-                this.outToClient.writeBytes(message);
-            }catch(Exception e) {
-
+        
+        /**
+         * Sendet Nachricht an Client
+         * 
+         * @param message
+         */
+        public void writeToClient(String message)
+        {
+            try
+            {
+                outToClient.writeBytes(message);
+            }
+            catch (Exception e)
+            {
+                e.getMessage();
             }
         }
-
+        
+        /**
+         * Schließt alle Input/Output-Streams und das Socket
+         */
         public void closeSocket()
         {
-            try {
+            try
+            {
                 if (inFromClient != null)
                     inFromClient.close();
-                if (outToClient != null) outToClient.close();
-                if (clientSocket != null) clientSocket.close();
-            } catch (IOException e) {
+                if (outToClient != null)
+                    outToClient.close();
+                if (clientSocket != null)
+                    clientSocket.close();
+            }
+            catch (IOException e)
+            {
                 e.printStackTrace();
             }
         }
